b651faa2225fc1787d514c7ee4bbb224537bef32
James Yong <me@jamesyong42.com>
2026-02-22 20:17:45 -0800

    Initial Commit

---
+++ Mochi/MochiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MochiApp.swift
+//  Mochi
+//
+//  Created by James Yong on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct MochiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MochiTests/MochiTests.swift
@@ -0,0 +1,17 @@
+//
+//  MochiTests.swift
+//  MochiTests
+//
+//  Created by James Yong on 2/22/26.
+//
+
+import Testing
+@testable import Mochi
+
+struct MochiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
